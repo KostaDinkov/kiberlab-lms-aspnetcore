457f1ee98d44c57f1d8c54bbffea9e78877fe1c0
Kosta <k.v.dinkov@gmail.com>
2020-11-02 23:51:55 +0200

    add: CRUD operations for courses

---
+++ BasicTemplate/KiberlabLMS/KiberlabLMS/Controllers/CoursesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using KiberlabLMS.Data;
+using KiberlabLMS.Models.CourseModels;
+using KiberlabLMS.ViewModels.Course;
+
+namespace KiberlabLMS.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CoursesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Courses
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Courses.ToListAsync());
+        }
+
+        // GET: Courses/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses.Include(c=>c.Lessons)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // GET: Courses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Courses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create( CourseViewModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                var course = new Course
+                {
+                    Name = input.Name,
+                    Description = input.Description,
+                    CourseCode = input.CourseCode
+                };
+                _context.Add(course);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(input);
+        }
+
+        // GET: Courses/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            var model = new CourseViewModel { Id = course.Id, Name= course.Name, Description = course.Description, CourseCode = course.CourseCode};
+            return View(model);
+        }
+
+        // POST: Courses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id,  CourseViewModel model)
+        {
+            if (id != model.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var course = this._context.Courses.FirstOrDefault(c => c.Id == id);
+                    
+                    course.Name = model.Name;
+                    course.Description = model.Description;
+                    course.CourseCode = model.CourseCode;
+
+                    _context.Update(course);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CourseExists(model.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        // GET: Courses/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // POST: Courses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CourseExists(string id)
+        {
+            return _context.Courses.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ BasicTemplate/KiberlabLMS/KiberlabLMS/ViewModels/Course/CourseViewModel.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.Security.AccessControl;
+
+namespace KiberlabLMS.ViewModels.Course
+{
+    public class CourseViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(20,MinimumLength = 5)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 10)]
+        public string Description { get; set; }
+
+        [Required]
+        public string CourseCode { get; set; }
+    }
+}
