42e4f255672260a8779e1db07553083c2187c44d
Kosta <k.v.dinkov@gmail.com>
2020-11-03 13:49:23 +0200

    add: move lessonsList partial to a view component

---
+++ BasicTemplate/KiberlabLMS/KiberlabLMS/Components/LessonsList.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Linq;
+using System.Threading.Tasks;
+using KiberlabLMS.Models.CourseModels;
+using KiberlabLMS.ViewModels.Course;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KiberlabLMS.Components
+{
+    public class LessonsListViewComponent:ViewComponent
+    {
+        public  IViewComponentResult Invoke(Course course)
+        {
+            var courseLessonsModel = new CourseLessonsViewModel
+            {
+                Course = course,
+                CourseId = course.Id,
+                Lessons = course.Lessons.OrderBy(lesson => lesson.Position).ToList(),
+            };
+            return this.View(courseLessonsModel);
+        }
+    }
+}
